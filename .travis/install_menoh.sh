392028c0f4582e55ad652891637b6db9b7399493
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-07-04 19:13:00 +0900

    travis: add osx build

--- .travis/install_menoh.sh
+++ .travis/install_menoh.sh
@@ -5,11 +5,20 @@ if [ ! -d "$HOME/menoh${MENOH_INSALL_SUFFIX}/lib" ]; then
     git checkout $MENOH_REV
     mkdir -p build
     cd build
-    cmake \
-      -DMKLDNN_INCLUDE_DIR="$HOME/mkl-dnn${MKL_DNN_INSALL_SUFFIX}/include" \
-      -DMKLDNN_LIBRARY="$HOME/mkl-dnn${MKL_DNN_INSALL_SUFFIX}/lib/libmkldnn.so" \
-      -DCMAKE_INSTALL_PREFIX=$HOME/menoh${MENOH_INSALL_SUFFIX} \
-      ..
+    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+      cmake \
+        -DENABLE_TEST=OFF -DENABLE_BENCHMARK=OFF -DENABLE_EXAMPLE=OFF -DENABLE_TOOL=OFF \
+        -DMKLDNN_INCLUDE_DIR="$HOME/mkl-dnn${MKL_DNN_INSALL_SUFFIX}/include" \
+        -DMKLDNN_LIBRARY="$HOME/mkl-dnn${MKL_DNN_INSALL_SUFFIX}/lib/libmkldnn.so" \
+        -DCMAKE_INSTALL_PREFIX=$HOME/menoh${MENOH_INSALL_SUFFIX} \
+        ..
+    else
+      cmake \
+        -DENABLE_TEST=OFF -DENABLE_BENCHMARK=OFF -DENABLE_EXAMPLE=OFF -DENABLE_TOOL=OFF \
+        -DCMAKE_INSTALL_PREFIX=$HOME/menoh${MENOH_INSALL_SUFFIX} \
+        -DCMAKE_INSTALL_NAME_DIR=$HOME/menoh${MENOH_INSALL_SUFFIX}/lib \
+        ..
+    fi
     make
     make install
     cd ../..
