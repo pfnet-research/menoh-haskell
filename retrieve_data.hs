ba1a131c35b1776aedd665f234e5620eaa264488
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-11-30 09:30:35 +0900

    update VGG16 example

--- retrieve_data.hs
+++ retrieve_data.hs
@@ -12,6 +12,6 @@ main = do
         putStrLn $ req ++ " -> " ++ fname
         request <- parseRequest req
         runResourceT $ httpSink request $ \_ -> sinkFile fname
-  downloadTo "https://www.dropbox.com/s/bjfn9kehukpbmcm/VGG16.onnx?dl=1" "./data/VGG16.onnx"
+  downloadTo "https://preferredjp.box.com/shared/static/o2xip23e3f0knwc5ve78oderuglkf2wt.onnx" "./data/vgg16.onnx"
   downloadTo "https://raw.githubusercontent.com/HoldenCaulfieldRye/caffe/master/data/ilsvrc12/synset_words.txt" "./data/synset_words.txt"
   downloadTo "https://upload.wikimedia.org/wikipedia/commons/5/54/Light_sussex_hen.jpg" "./data/Light_sussex_hen.jpg"
